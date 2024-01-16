8cc9df7b2347c0b3dbddf77414ba86e5a71aecbd
mspendyala <100178338+mspendyala@users.noreply.github.com>
2024-01-15 23:02:20 -0600

    Update NewCW.py

--- utils/NewClassWork/NewCW.py
+++ utils/NewClassWork/NewCW.py
@@ -4,7 +4,7 @@ import subprocess
 from pathlib import Path
 
 current_date = arrow.now().format('YYYY-MMM-DD-HH-mm')
-file_path = Path("C:\\Users\\mp71517\\OneDrive-Deere&Co\\OneDrive - Deere & Co\\Documents\\ClassWork\\2024")
+file_path = Path("C:\\Users\\mp\\Documents\\ClassWork\\2024")
 
 file_name = current_date + str('.yml')
 full_file_path = os.path.join(file_path, file_name)
